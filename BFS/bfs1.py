dde1260fb732e2c819e42442f542f172b9bd9217
yerim <chung2134@gmail.com>
2025-08-31 22:05:09 +0900

    문제 오답 수정

--- BFS/bfs1.py
+++ BFS/bfs1.py
@@ -8,14 +8,16 @@ n,m,v = map(int,input().split())
 A = [[] for _ in range(n+1)]
 visited = [False] * (n+1)
 
-
 for _ in range(m):
     x,y = map(int,input().split())
     A[x].append(y)
     A[y].append(x)
 
+for i in range(1, n+1):
+    A[i].sort()
 
 def DFS(v):
+    print(v, end= ' ')
     visited[v] = True
     for i in A[v]:
         if not visited[i]:
@@ -26,7 +28,13 @@ def BFS(v):
     visited[v] = True
     while queue:
         now = queue.popleft()
+        print(now, end= ' ')
         for i in A[now]:
             if not visited[i]:
                 visited[i] = True
                 queue.append(i)
+
+DFS(v)
+visited = [False] * (n+1)
+print()
+BFS(v)
